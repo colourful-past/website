c85d846e17db2d2bfff9f6d5e01538977c38d324
Hourann Bosci <hourannb@dview.net>
2016-07-30 18:59:55 +0800

    also deploy to new second server

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,23 +4,31 @@
 # - sudo npm install -g gulp-cli
 # - sudo npm install -g tsd
 
-ssh ubuntu@colourfulpast.org /bin/bash -e <<REMOTE_COMMANDS
-    cd /home/ubuntu
-    sudo rm -rf website
+SERVERS = ( 52.42.187.96 52.41.100.249 )
 
-    git clone git@github.com:colourful-past/website.git
-    cd website
+for SERVER in "${SERVERS[@]}"
+do
+    echo "----------------------------------------"
+    echo "Deploying to $SERVER"
+    echo "----------------------------------------"
 
-    npm install
-    tsd install
-    /usr/bin/gulp release
+    ssh ubuntu@$SERVER /bin/bash -e <<REMOTE_COMMANDS
+        cd /home/ubuntu
+        sudo rm -rf website
 
-    sudo cp -f /home/ubuntu/website/deploy/nginx-config /etc/nginx/sites-available/website
-    sudo ln -sf /etc/nginx/sites-available/website /etc/nginx/sites-enabled/website
-    sudo rm -f /etc/nginx/sites-enabled/default
-    sudo service nginx restart
+        git clone git@github.com:colourful-past/website.git
+        cd website
 
-    sudo cp -f /home/ubuntu/website/deploy/upstart.conf /etc/init/website.conf
-    sudo service website restart
+        npm install
+        tsd install
+        /usr/bin/gulp release
+
+        sudo cp -f /home/ubuntu/website/deploy/nginx-config /etc/nginx/sites-available/website
+        sudo ln -sf /etc/nginx/sites-available/website /etc/nginx/sites-enabled/website
+        sudo rm -f /etc/nginx/sites-enabled/default
+        sudo service nginx restart
+
+        sudo cp -f /home/ubuntu/website/deploy/upstart.conf /etc/init/website.conf
+        sudo service website restart
 
 REMOTE_COMMANDS
